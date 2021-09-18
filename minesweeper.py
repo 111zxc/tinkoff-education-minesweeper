af6fd9e1a59695f9be66e77cf853c10e2bce51dd
111zxc <111zxcqqq@gmail.com>
2021-09-18 22:55:03 +0300

    Modified minesweeper.py
    Added user CMD handling

--- minesweeper.py
+++ minesweeper.py
@@ -1,12 +1,18 @@
 import random
+import re
+import os
 
 
 def main():
+    # os.system("cls")
+    open_tiles = 0
+    alive = True
+
     try:
         m, n = map(int, input("[?] Введите размеры поля (через пробел): ").split())
     except ValueError:
         raise ValueError("[!] Вы ввели неверные размеры поля!")
-    if m*n <= 1 or m < 0 or n < 0:
+    if m * n <= 1 or m < 0 or n < 0:
         raise Exception("[!] Поле должно иметь больше одной клетки!")
 
     try:
@@ -16,17 +22,30 @@ def main():
     if mine_count <= 0 or mine_count >= m * n:
         raise Exception("[!] Мин должно быть больше нуля и меньше, чем клеток на поле!")
 
-    player_field = [[0 for i in range(n)] for j in range(m)]
-    answer_field = [[0 for i in range(n)] for j in range(m)]
+    player_field = [[0 for i in range(m)] for j in range(n)]
+    answer_field = [[0 for i in range(m)] for j in range(n)]
 
     for _ in range(mine_count):
-        i = random.randint(0, m-1)
-        j = random.randint(0, n-1)
+        i = random.randint(0, m - 1)
+        j = random.randint(0, n - 1)
 
         if answer_field[i][j] == 0:
             answer_field[i][j] = 1
         else:
             _ -= 1
+
+    while open_tiles < m * n - mine_count and alive:
+        # os.system("cls")
+        user_cmd = [0, 0, "Action"]
+        print(player_field)  # TODO: заменить show_field()
+        print("[?] Введите ваше следующее действие в формате [X, Y, Flag/Open]: ")
+        user_input = input()  # TODO: снизу полный пиздец
+        if re.match(r"\[[0-9]+, [0-9]+, (Flag|Open)\]", user_input) \
+                and 0 <= int(re.findall(r"[0-9]+", user_input)[0]) < m \
+                and 0 <= int(re.findall(r"[0-9]+", user_input)[1]) < n:
+            user_cmd = [int(re.findall(r"[0-9]+", user_input)[0]), int(re.findall(r"[0-9]+", user_input)[1]), user_input[-5:-1]]
+        else:
+            print("[!] Вы ввели неправильную команду!")
 
 
 if __name__ == "__main__":
