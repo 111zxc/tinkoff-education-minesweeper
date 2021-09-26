846e43453f339614803b51e14bc21c88ae8b2d3a
111zxc <76816255+111zxc@users.noreply.github.com>
2021-09-26 16:10:48 +0300

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -1,12 +1,8 @@
 import random
-import re
 import os
 from time import sleep
 
 def main():
-    encode_key = 'tinkovf-edu_zxc1'  # ключ для шифрования сохранения
-    dictionary = '0123456789abcdef'
-
     # получить координаты всех соседних клеток
     def get_all_near_tiles(i, j, n, m):
         answer = []
@@ -33,98 +29,16 @@ def main():
                     player_field[i][j] = "-"  # возвращаем дэш, если рядом 0 мин
                     answer_field[i][j] = 2
                     for element in get_all_near_tiles(i, j, n, m):  # если рядом нет мин, открываем все клетки поблизости
-                        answer_field, player_field, alive = open_tile(element[0], element[1], answer_field, player_field, alive, limit_counter)
-                save_game()
-                clear()
+                        open_tile(element[0], element[1], answer_field, player_field, alive, limit_counter)
         elif player_field[i][j] == "x":
             # не даём открывать клетки, помеченные флагом. Надеюсь, так надо
-            clear()
             print("[?] На эту клетку вы поставили флаг. Снимите его, чтобы раскрыть содержимое этой клетки.")
-            save_game()
         else:
-            clear()
-            print("[?] Вы уже открывали эту клетку.")  # хэндл открытия уже открытой клетки
-            save_game()        
+            print("[?] Вы уже открывали эту клетку.")  # хэндл открытия уже открытой клетки       
         return answer_field, player_field, alive
 
     def clear():  # очистить экран
         os.system("cls" if os.name == 'nt' else "clear")
-
-    def load_game():  # загрузка сэйва
-        f = open("sa.ve")
-        content = f.readlines()
-
-        # получение размерности поля из сэйва
-        size = content[0][:-1]
-        m, n = int(size[0:size.index("*")]), int(size[size.index("*")+1:])
-
-        # расшифровка матрицы ответов
-        answers = ""
-        for symbol in content[1]:
-            answers += dictionary[encode_key.index(symbol)]
-        answers = str(int(answers, 16))
-
-        # вычитаем прибавленные при сохранении единицы
-        temp = ""
-        for symbol in answers:
-            temp += str(int(symbol) - 1)
-        answers = temp
-
-        open_tiles = answers.count("2")
-        mine_count = answers.count("1")
-        
-        answer_field = [[0 for i in range(m)] for j in range(n)]
-        player_field = [[0 for i in range(m)] for j in range(n)]
-        
-        # заполнение поля ответов
-        cntr = 0
-        for i in range(n):
-            for j in range(m):
-                answer_field[i][j] = int(answers[cntr])
-                cntr += 1
-
-        # основываясь на ответах, заполняет поле игрока
-        for i in range(n):
-            for j in range(m):
-                if answer_field[i][j] == (0 or 1):
-                    player_field[i][j] = 0
-                if answer_field[i][j] == 3:
-                    player_field[i][j] = "x"
-                if answer_field[i][j] == 4:
-                    player_field[i][j] = "x"
-                if answer_field[i][j] == 2:
-                    player_field[i][j] = "-" if get_nearby_mines(n, m, i, j, answer_field) == 0 else get_nearby_mines(n, m, i,j, answer_field)
-        f.close()
-
-        alive = True
-
-        print("[$] Игра успешно загружена!")
-        return m, n, mine_count, open_tiles, answer_field, player_field
-
-    # очистить сэйв (после проигрыша или победы)
-    def clear_save():
-        file = open("sa.ve", "w")
-        file.close()
-
-    def save_game():
-        file = open("sa.ve", "w")
-        
-        answers_encoded = ""
-        answers = ""
-        
-        # записываем значения клеток в строку
-        for i in range(n):
-            for j in range(m):
-                answers += str(answer_field[i][j] + 1)  # +1 во избежание проблемы с нулями в начале матрицы
-        
-        # переводим ответы в 16сс и шифруем согласно ключу
-        answers = hex(int(answers))[2:]
-        for symbol in answers:
-            answers_encoded += encode_key[dictionary.index(symbol)]
-        
-        # сохранение размеров матрицы и зашифрованных ответов
-        file.write(f"{m}*{n}\n{answers_encoded}")
-        file.close()
 
     # получить количество мин по-соседству с данной клеткой; используется после открытия клетки
     def get_nearby_mines(n, m, i, j, answer_field):
@@ -146,98 +60,102 @@ def main():
 
     open_tiles = 0 # количество открытых клеток для учета состояния игры
     alive = True
-
-    f = open("sa.ve")
-    save_info = f.read() 
-    f.close()
-
-    if save_info == "": # если сэйва нет
-        alive = True
         
-        # ввод размерностей поля
-        try:
-            n, m = map(int, input("[?] Введите размеры поля (через пробел): ").split())
-        except ValueError:
-            raise ValueError("[!] Вы ввели неверные размеры поля!")
-        if m * n <= 1 or m < 0 or n < 0:
-            raise Exception("[!] Поле должно иметь больше одной клетки!")
+    # ввод размерностей поля
+    try:
+        n, m = map(int, input("[?] Введите размеры поля (через пробел): ").split())
+    except ValueError:
+        raise ValueError("[!] Вы ввели неверные размеры поля!")
+    if m * n <= 1 or m < 0 or n < 0:
+        raise Exception("[!] Поле должно иметь больше одной клетки!")
 
-        # ввод количества мин
-        try:
-            mine_count = int(input("[?] Введите количество мин: "))
-        except ValueError:
-            raise ValueError("[!] Вы ввели неверное количество мин!")
-        if mine_count <= 0 or mine_count >= m * n:
-            raise Exception("[!] Мин должно быть больше нуля и меньше, чем клеток на поле!")
+    # ввод количества мин
+    try:
+        mine_count = int(input("[?] Введите количество мин: "))
+    except ValueError:
+        raise ValueError("[!] Вы ввели неверное количество мин!")
+    if mine_count <= 0 or mine_count >= m * n:
+        raise Exception("[!] Мин должно быть больше нуля и меньше, чем клеток на поле!")
 
-        player_field = [[0 for i in range(m)] for j in range(n)]  # матрица поля, отображаемого игроку
-        answer_field = [[0 for i in range(m)] for j in range(n)]  # матрица поля с ответами
+    player_field = [[0 for i in range(m)] for j in range(n)]  # матрица поля, отображаемого игроку
+    answer_field = [[0 for i in range(m)] for j in range(n)]  # матрица поля с ответами
 
-        # генерация мин
-        for _ in range(mine_count):
-            i = random.randint(0, m - 1)
-            j = random.randint(0, n - 1)
+    # генерация мин
+    for _ in range(mine_count):
+        i = random.randint(0, m - 1)
+        j = random.randint(0, n - 1)
 
-            if answer_field[j][i] == 0:
-                answer_field[j][i] = 1
-            else:
-                _ -= 1
-        save_game()
-    else:
-        m, n, mine_count, open_tiles, answer_field, player_field = load_game()
+        if answer_field[j][i] == 0:
+            answer_field[j][i] = 1
+        else:
+            _ -= 1
 
     # цикл игры; работает, пока мы живы и существуют неоткрытые клетки, не являющиеся минами
     while open_tiles < m * n - mine_count and alive:
-
-        user_cmd = [0, 0, "Action"]  # [X, Y, (Flag|Open)]
         show_field()
+
+        # действие алгоритма
+        value_map = [[0 for i in range(m)] for j in range(n)]
+        tempCounter = 0
+        for i in player_field:
+            for j in i:
+                if j == 0:
+                    tempCounter += 1
+
+        if tempCounter == m*n:
+            # хитмап для первого хода
+            for i in range(n):
+                for j in range(m):
+                    nearby_tiles = len(get_all_near_tiles(i, j, n, m))
+                    value_map[i][j] = 1 - (mine_count/(n*m)) + nearby_tiles
+        else:  # хитмап для последующих ходов
+            for i in range(n):
+                for j in range(m):
+                    if player_field[i][j] != 0:  # если клетка уже открыта, мы не будем ее открывать
+                        value_map[i][j] = -99
+                    else:
+                        value_map[i][j] = 1
+                        
+                        closed_tiles_nearby = 0
+                        for el in get_all_near_tiles(i, j, n, m):
+                            if player_field[el[0]][el[1]] == 0:
+                                closed_tiles_nearby += 1
+                        value_map[i][j] += closed_tiles_nearby/(m*n-open_tiles)
+
+                        for el in get_all_near_tiles(i, j, n, m):
+                            if player_field[el[0]][el[1]] != 0 and player_field[el[0]][el[1]] != '-':
+                                x = int(player_field[el[0]][el[1]])
+                                num_closed_tiles = 0
+                                for el2 in get_all_near_tiles(el[0], el[1], n, m):
+                                    if player_field[el2[0]][el2[1]] == 0:
+                                        num_closed_tiles += 1
+                                value_map[i][j] -= x/num_closed_tiles
+        # выбор самой выгодной клетки (max)
+        maxel = -1000
+        el_i, el_j = 0, 0
+        for i in range(n):
+            for j in range(m):
+                if value_map[i][j] > maxel and value_map != -99:
+                    maxel = value_map[i][j]
+                    el_i = i
+                    el_j = j
+        print(f"[?] Алгоритм решил выбрать клетку с координатами ({el_i}, {el_j})!")
+        open_tile(el_i, el_j, answer_field, player_field, alive)
+        ## конец работы алгоритма ##
         
-        print("[?] Введите ваше следующее действие в формате [X, Y, Flag/Open]: ")
-        user_input = input()
-
-        # проверка пользовательского ввода
-        if re.match(r"\[[0-9]+, [0-9]+, (Flag|Open)\]", user_input) \
-                and 0 <= int(re.findall(r"[0-9]+", user_input)[1]) < n \
-                and 0 <= int(re.findall(r"[0-9]+", user_input)[0]) < m:
-            user_cmd = [n - 1 - int(re.findall(r"[0-9]+", user_input)[1]), int(re.findall(r"[0-9]+", user_input)[0]),
-                        user_input[-5:-1]]
-
-            if user_cmd[2] == 'Flag':  # если Action = Flag
-                if player_field[user_cmd[0]][user_cmd[1]] != "x":  # если клетка еще не открыта и не помечена флагом
-                    player_field[user_cmd[0]][user_cmd[1]] = "x"  # помечаем клетку флагом
-                    if answer_field[user_cmd[0]][user_cmd[1]] == 1:
-                        answer_field[user_cmd[0]][user_cmd[1]] = 3
-                    else:
-                        answer_field[user_cmd[0]][user_cmd[1]] = 4
-                    clear()
-                    save_game()
-                else:
-                    player_field[user_cmd[0]][user_cmd[1]] = 0  # если клетка уже помечена флагом, снимаем флаг
-                    if answer_field[user_cmd[0]][user_cmd[1]] == 3:
-                        player_field[user_cmd[0]][user_cmd[1]] = 1
-                    else:
-                        player_field[user_cmd[0]][user_cmd[1]] = 0
-                    clear()
-                    save_game()
-            elif user_cmd[2] == 'Open':  # если Action = Open
-                answer_field, player_field, alive = open_tile(user_cmd[0], user_cmd[1], answer_field, player_field, alive)
-        else:
-            clear()
-            print("[!] Вы ввели неправильную команду!")  # хэндл неправильного ввода
         open_tiles = 0
         for i in range(n):
             for j in range(m):
-                if answer_field[i][j] == 2:
-                    open_tiles += 1
+                if answer_field[i][j] == 2: open_tiles += 1
+                        
 
     if alive:  # винкондишн достигается, если цикл вайл закончен (нет неоткрытых клеток не мин) и мы остались в живых
-        clear()
-        print("[$] Вы победили!")
+        print("[$] Алгоритм победил!")
+        show_field()
     else:
-        print("[!] К сожалению, Вы взорвались..")
-    print("[?] Следующая игра начнётся через 10 секунд..    ")
-    clear_save()
-    sleep(10)
+        print("[!] К сожалению, даже алгоритм проиграл..")
+    print("[?] Следующая игра начнётся через 100 секунд..")
+    sleep(100)
     main()
 
 
